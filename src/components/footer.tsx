ac3272d61cb661f93c1ea5c58a5a64faa90400da
martim <martimkauan156@gmail.com>
2024-12-03 20:16:56 -0300

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import { Separator } from "@/components/ui/separator";
+
+
+export const Footer = () => {
+    return (
+        <footer className="mt-5">
+            <Separator />
+            <div className="my-5 text-center text-sm opacity-50">
+                Criado por Kauan Martim
+            </div>
+        </footer>
+    );
+}
